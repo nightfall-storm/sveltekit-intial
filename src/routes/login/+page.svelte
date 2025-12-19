fb2705ef02247ce2a75407db5fa582c3c6b883fb
nightfall-storm <bilalnnassernf@gmail.com>
2025-12-19 16:57:35 +0100

    initial commit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Login</title>
+	<meta name="description" content="Login page" />
+</svelte:head>
+
+<section>
+	<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+		<div class="flex w-full max-w-md flex-col gap-6"></div>
+	</div>
+</section>
